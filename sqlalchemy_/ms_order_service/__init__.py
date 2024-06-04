eb3969ead23b0ef53a4c3ea0078270edd944ab58
Chen-Xueer <xueer.chen@liteon.com>
2024-06-04 17:16:54 +0800

    update build.yaml

--- sqlalchemy_/ms_order_service/__init__.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy_.ms_order_service.base import Base
-from sqlalchemy_.ms_order_service.order import Order 
-from sqlalchemy_.ms_order_service.transaction import Transaction
-from sqlalchemy_.ms_order_service.tenant import Tenant
